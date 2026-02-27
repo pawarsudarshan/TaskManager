d13322af5cda5ffdf41cf82c6bccb33dbf4dbb28
Ubuntu <sudarshan@vm-suda-dev.h5wgmll53vnenige3hk5lwqmwe.rx.internal.cloudapp.net>
2026-02-27 18:32:49 +0000

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {}
