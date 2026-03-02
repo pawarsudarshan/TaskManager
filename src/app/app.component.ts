68d8ab47bd849b1cc9bc475a0c1132fe73402484
Ubuntu <sudarshan@vm-suda-dev.h5wgmll53vnenige3hk5lwqmwe.rx.internal.cloudapp.net>
2026-03-02 14:32:22 +0000

    Passed down values from parent to child and other way round as well using Input and Output decorators in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserComponent } from './user/user.component';
 import { HeaderComponent } from './header/header.component';
 import { DUMMY_USERS } from './dummy-users';
+import { consumerPollProducersForChange } from '@angular/core/primitives/signals';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,7 @@ import { DUMMY_USERS } from './dummy-users';
 
 export class AppComponent {
   users = DUMMY_USERS;
+  onSelectUser(id: string) {
+    console.log("Selected user with id" + id);
+  }
 }
