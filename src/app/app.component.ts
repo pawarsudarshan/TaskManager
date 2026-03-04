b8012436f3048efb6a1ee68db04e21bced08ebd0
sudarshan-suryakant-pawar <sudarshan-suryakant.pawar@capgemini.com>
2026-03-04 11:40:11 +0530

    outputting user's name upon clicking the user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,14 @@ import { TasksComponent } from "./tasks/tasks.component";
 
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1'
+
   onSelectUser(id: string) {
-    console.log("Selected user with id" + id);
+    this.selectedUserId = id;
   }
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
 }
